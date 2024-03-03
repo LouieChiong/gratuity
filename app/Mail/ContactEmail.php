85d88073cd7a06eda7832d529203f62304ece5c3
louie chiong <louiechiong@gmail.com>
2024-03-03 14:06:21 +0800

    change to laravel application

---
+++ app/Mail/ContactEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Inquiry Message')
+            ->from('info@gratuityglobal.org')
+            ->view('mail.contact', [
+                'fullname' => $this->data['first_name'] . ' ' . $this->data['last_name'],
+                'email' => $this->data['email'],
+                'content' => $this->data['message'],
+                'contact' => $this->data['contact_number'],
+            ]);
+    }
+}
