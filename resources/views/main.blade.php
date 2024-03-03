52609696d9b9290b7f040faf61a5cc72d4c6c277
louie chiong <louiechiong@gmail.com>
2024-03-03 14:33:10 +0800

    add resources in api routes vercel

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,7 +13,6 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-icons.css" rel="stylesheet">
         <link href="css/templatemo-kind-heart-charity.css" rel="stylesheet">
-
     </head>
 
     <body id="section_1 position-relative">
