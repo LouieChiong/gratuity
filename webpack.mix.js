92f8db0e363f0b5ad0989308c3f68b5e8865cb5a
louie chiong <louiechiong@gmail.com>
2024-03-03 14:54:37 +0800

    add resrouces files into webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .copy('resources/css/', 'public/css')
+    .copy('resources/js/', 'public/js')
+    .copy('resources/fonts/', 'public/fonts')
+    .copy('resources/images/', 'public/images')
     .postCss('resources/css/app.css', 'public/css', [
-        //
     ]);
