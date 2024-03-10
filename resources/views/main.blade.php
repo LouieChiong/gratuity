9eedc839645579e153f486427279f07d22ba45f7
louieAnj <louie_chiong@spice-factory.ph>
2024-03-10 17:32:40 +0800

    use cdn for javascript

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="og:title" content="ratuity - Digital Marketing and Pulishing" />
         <meta property="og:description" content="Empower authors, from aspiring writers to seasoned wordsmiths, by providing a comprehensive and collaborative platform for the creation, publication, and promotion of their literary works." />
-        <meta property="og:image" content="images/logo.png" />
+        <meta property="og:image" content="{{ Vite::asset('resources/images/logo.png') }}" />
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha512-SFaNb3xC08k/Wf6CRM1J+O/vv4YWyrPBSdy0o+1nqKzf+uLrIBnaeo8aYoAAOd31nMNHwX8zwVwTMbbCJjA8Kg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         @vite([
             'resources/css/bootstrap.min.css',
@@ -291,12 +295,10 @@
         </footer>
 
         @vite([
-            'resources/js/bootstrap.min.js',
+            'resources/js/jquery.sticky.js',
             'resources/js/click-scroll.js',
             'resources/js/counter.js',
             'resources/js/custom.js',
-            'resources/js/jquery.min.js',
-            'resources/js/jquery.sticky.js',
         ])
     </body>
 </html>
--- resources/views/marketing.blade.php
+++ resources/views/marketing.blade.php
@@ -8,7 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="og:title" content="ratuity - Digital Marketing and Pulishing" />
         <meta property="og:description" content="Empower authors, from aspiring writers to seasoned wordsmiths, by providing a comprehensive and collaborative platform for the creation, publication, and promotion of their literary works." />
-        <meta property="og:image" content="images/logo.png" />
+        <meta property="og:image" content="{{ Vite::asset('resources/images/logo.png') }}" />
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha512-SFaNb3xC08k/Wf6CRM1J+O/vv4YWyrPBSdy0o+1nqKzf+uLrIBnaeo8aYoAAOd31nMNHwX8zwVwTMbbCJjA8Kg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         @vite([
             'resources/css/bootstrap.min.css',
@@ -173,7 +177,7 @@
             </section>
         </main>
 
-        <footer class="site-footer">
+        <footer class="site-footer" id="">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-12">
@@ -184,10 +188,16 @@
                         <!-- Adding the 'text-end' class to align content to the right -->
                         <h5 class="site-footer-title mb-3 text-white">Contact Information</h5>
                         <p class="text-white mb-2">
+                            <i class="bi-envelope me-2"></i>
+                            <span>bpreston@gratuityglobal.org</span>
+                        </p>
+                        <p class="text-white mb-2">
                             <i class="bi-telephone me-2"></i>
-                            <a href="tel:120-240-9600" class="site-footer-link">
-                                (888) 290-0987
-                            </a>
+                            <span> (888) 290-0987</span>
+                        </p>
+                        <p class="text-white mb-2">
+                            <i class="bi-telephone-forward me-2"></i>
+                            <span> 372-712-1136</span>
                         </p>
                         <p class="text-white mt-3">
                             <i class="bi-geo-alt me-2"></i>
@@ -210,13 +220,10 @@
         </footer>
 
         @vite([
-            'resources/js/bootstrap.min.js',
+            'resources/js/jquery.sticky.js',
             'resources/js/click-scroll.js',
             'resources/js/counter.js',
             'resources/js/custom.js',
-            'resources/js/jquery.min.js',
-            'resources/js/jquery.sticky.js',
         ])
-
     </body>
 </html>
