da4d23a1d8652b9795fe7d878a707aba9ede6bc3
louieAnj <louie_chiong@spice-factory.ph>
2024-03-31 23:22:04 +0800

    publishing details pages

---
+++ resources/views/publishing/competent.blade.php
@@ -0,0 +1,87 @@
+@extends('components.layout')
+@section('content')
+<div class="d-flex text-center justify-content-center align-items-center row">
+    <div class="col-lg-8 col-md-8 gap-2">
+        <h2>COMPETENT</h2>
+        <p style="text-align: justify;">
+            Our most wide-ranging publishing package. It is an excellent choice for authors who want to offer their published books to the
+            marketplace. Our <strong>COMPETENT</strong> Package is recommended to those who want to make their book a stern candidate in today's competitive publishing environment.        </p>
+        <div class="d-flex text-center justify-content-start align-items-start">
+            <ul class="custom-list">
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Cover and Interior Customization
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    ISBN Assignment
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Hardcover Availability
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Paperback Availability
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    ePub/eBook Availability
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Global Distribution
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Library of Congress Control Number
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Copyright Registration
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Unlimited Color/ Black and White Image Insertions
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Amazon “ Look Inside” and Google Preview
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Barnes and Noble “ Read Instantly” Feature
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Free Basic Editing of up to 35,000 words
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    In-depth Editorial Evaluation
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Booksellers Return Program
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    iPage Listing Advertising
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Author Website Set-Up
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    15 Paperback Copies
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    5 Hardcover Copies
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/publishing/foundational.blade.php
@@ -0,0 +1,59 @@
+@extends('components.layout')
+@section('content')
+<div class="d-flex text-center justify-content-center align-items-center row">
+    <div class="col-lg-8 col-md-8 gap-2">
+        <h2>FOUNDATIONAL</h2>
+        <p style="text-align: justify;">
+            Our most economical <b>FOUNDATIONAL </b> program is equipped with all the essentials for an efficient international publication. It also gives you the freedom and flexibility to choose add on services you’d like to get at anytime in the publication process.
+        </p>
+        <div class="d-flex text-center justify-content-start align-items-start">
+            <ul class="custom-list">
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Cover and Interior Customization
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    ISBN Assignment
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Paperback Availability
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    ePub/eBook Availability
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Global Distribution
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Library of Congress Control Number
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Copyright Registration
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Unlimited Color/ Black and White Image Insertions
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Amazon “ Look Inside” and Google Preview
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    Barnes and Noble “ Read Instantly” Feature
+                </li>
+                <li class="custom-list-item d-flex" style="font-size: 17px">
+                    <i class="bi-check custom-text-box-icon me-2"></i>
+                    5 Free Paperback Copies
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
